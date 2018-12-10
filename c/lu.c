2c35fadcae8cea040a61981ba006b2f61e554592
guenchi <louis.deguenchy@gmail.com>
2018-12-10 03:01:53 +0100

    Update lu.c

---
+++ c/lu.c
@@ -0,0 +1,3 @@
+#include "/usr/local/Cellar/luajit/2.0.5/include/luajit-2.0/lua.h"
+#include "/usr/local/Cellar/luajit/2.0.5/include/luajit-2.0/lualib.h"
+#include "/usr/local/Cellar/luajit/2.0.5/include/luajit-2.0/lauxlib.h"
